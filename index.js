305dedf4750ce2f6b8fc1be694906d46f5135037
木戸 康平 <bouningenn@gmail.com>
2021-01-14 11:05:54 +0900

    expose obniz class

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 let Noble = require('./lib/noble');
+let Obniz = require('obniz');
 let bindings = require('./lib/obniz-hci-socket/bindings');
 let nobles = {};
 
-function idFilter(obnizId){
+function idFilter(obnizId) {
   let str = "" + obnizId;
   return str.split("-").join("");
 }
 
-module.exports = (obnizId, params)=>{
-    let id = idFilter(obnizId);
-    if(!nobles[id]){
-      let bind = new bindings(obnizId, params);
-      nobles[id]  = new Noble(bind);
-      nobles[id].obniz = bind._obniz;
-    }
+const obnizNoble = (obnizId, params) => {
+  let id = idFilter(obnizId);
+  if (!nobles[id]) {
+    let bind = new bindings(obnizId, params);
+    nobles[id] = new Noble(bind);
+    nobles[id].obniz = bind._obniz;
+  }
 
-    return nobles[id] ;
-}
+  return nobles[id];
+};
+
+
+obnizNoble.Obniz = Obniz;
+
+module.exports = obnizNoble;
